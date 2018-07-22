6eec74971474e00bb3ef3c62dd7b2b55e6f66b8c
Grzegorz Józefowicz <39644658+gjozef84@users.noreply.github.com>
2018-07-22 22:46:08 +0200

    Add files via upload

---
+++ isPalindrom/src/main/java/IsPalindrom.java
@@ -0,0 +1,28 @@
+import java.security.InvalidParameterException;
+
+public class IsPalindrom {
+
+    public static void main(String[] args) {
+        String str = "iwi";
+
+        if (isPalindrom(str)) System.out.println(str + " jest palindromem");
+        else System.out.println(str + " nie jest palindromem");
+    }
+
+    public static boolean isPalindrom(String s) {
+
+        if (s == null || s.equals("")) {
+            throw new InvalidParameterException();
+        } else {
+            boolean result = false;
+
+            for (int i = 0; i < s.length(); i++) {
+
+                if (s.charAt(i) == s.charAt(s.length() - 1 - i)) result = true;
+                else return false;
+
+            }
+            return result;
+        }
+    }
+}
